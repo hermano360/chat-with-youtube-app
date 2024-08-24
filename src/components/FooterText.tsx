687a8038172565f6ae14836c6ceac9697b46e6a7
Herminio Garcia <hermano360@gmail.com>
2024-08-24 00:40:56 -0700

    Add Footer Text

---
+++ src/components/FooterText.tsx
@@ -0,0 +1,12 @@
+export const FooterText = () => {
+  return (
+    <div className="text-sm text-gray-700 sticky bottom-10 text-center bg-opacity-5 bg-gray-900">
+      <div>
+        Feedback or suggestions? Reach out to{" "}
+        <a href="mailto:hermano360@gmail.com" className="text-gray-400">
+          hermano360@gmail.com
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/components/PartnerPage.tsx
+++ src/components/PartnerPage.tsx
@@ -8,6 +8,7 @@ import { VideoClips } from "@/components/VideoClips";
 import { WelcomeMessage } from "@/components/WelcomeMessage";
 import { YoutubeEmbed } from "@/components/YoutubeEmbed";
 import { useVideoHandling } from "@/hooks";
+import { FooterText } from "./FooterText";
 
 export function PartnerPage({ username }: { username: string }) {
   const {
@@ -24,31 +25,34 @@ export function PartnerPage({ username }: { username: string }) {
   } = useVideoHandling();
 
   return (
-    <PageWrapper>
-      <WelcomeMessage username={username} />
+    <>
+      <PageWrapper>
+        <WelcomeMessage username={username} />
 
-      <QuestionInput
-        onStart={onStart}
-        onFinish={onFinish}
-        onError={onError}
-        username={username}
-      />
-      <LoadingIcon isLoading={isLoading} />
+        <QuestionInput
+          onStart={onStart}
+          onFinish={onFinish}
+          onError={onError}
+          username={username}
+        />
+        <LoadingIcon isLoading={isLoading} />
 
-      <Summary summary={summary} />
+        <Summary summary={summary} />
 
-      <YoutubeEmbed
-        embedId={embedId}
-        title={videoTitle}
-        onClear={() => setEmbedId("")}
-      />
+        <YoutubeEmbed
+          embedId={embedId}
+          title={videoTitle}
+          onClear={() => setEmbedId("")}
+        />
 
-      <VideoClips
-        clips={clips}
-        embedId={embedId}
-        setEmbedId={setEmbedId}
-        setVideoTitle={setVideoTitle}
-      />
-    </PageWrapper>
+        <VideoClips
+          clips={clips}
+          embedId={embedId}
+          setEmbedId={setEmbedId}
+          setVideoTitle={setVideoTitle}
+        />
+        <FooterText />
+      </PageWrapper>
+    </>
   );
 }
